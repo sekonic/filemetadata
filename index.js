d04e0476dc27b97490b5f96ff93ea82aada70c57
sekonic <sekonic@gmail.com>
2023-12-06 11:22:56 -0300

    Added multer and post file handling

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 4000;
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.post('/api/fileanalyse', upload.any(), (req, res) => {
+  let file = req.files[0];
+  res.status(200).json(
+    {
+      "name": file.originalname,
+      "type": file.mimetype,
+      "size": file.size
+    }
+  );
 });
+
+app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
